cee573865ea712823c76dbc29ff768dc80a38fa4
nirasan <niranirao@gmail.com>
2015-04-14 16:06:46 +0900

    question resolved

--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -6,6 +6,6 @@ describe BonusDrink do
     expect(BonusDrink.total_count_for(1)).to eq 1
     expect(BonusDrink.total_count_for(3)).to eq 4
     expect(BonusDrink.total_count_for(11)).to eq 16
-    expect(BonusDrink.total_count_for(100)).to eq 0
+    expect(BonusDrink.total_count_for(100)).to eq 149
   }
 end
